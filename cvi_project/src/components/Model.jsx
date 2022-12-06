e010932fa5903765c8cadcebb428b592d1c74a24
jose9806 <jose.d98@hotmail.com>
2022-12-06 06:28:24 -0500

    Add interactivity and models

---
+++ cvi_project/src/components/Model.jsx
@@ -0,0 +1,15 @@
+import {useLoader} from '@react-three/fiber';
+import {
+    GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader';
+const Model = props =>{
+    const model = useLoader(
+        GLTFLoader,
+        props.path
+    )
+    return (
+        <primitive object={model.scene} {...props}/>
+    )
+}
+
+export default Model;
