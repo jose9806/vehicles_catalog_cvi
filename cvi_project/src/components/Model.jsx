4a668e76811570b6b78fd709c2d4af9c71ccbc81
yilberth <yilberth@mavecure.co>
2022-12-06 08:21:44 -0500

    format documents

--- cvi_project/src/components/Model.jsx
+++ cvi_project/src/components/Model.jsx
@@ -1,15 +1,8 @@
-import {useLoader} from '@react-three/fiber';
-import {
-    GLTFLoader
-} from 'three/examples/jsm/loaders/GLTFLoader';
-const Model = props =>{
-    const model = useLoader(
-        GLTFLoader,
-        props.path
-    )
-    return (
-        <primitive object={model.scene} {...props}/>
-    )
-}
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+const Model = (props) => {
+  const model = useLoader(GLTFLoader, props.path);
+  return <primitive object={model.scene} {...props} />;
+};
 
 export default Model;
